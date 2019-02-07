6ea44d0fcb812ad70a9e4b116b8cc2e742b52b1e
htruong1 <htruong1@ualberta.ca>
2019-02-06 18:35:39 -0700

    added tavis for code coverage

--- src/test/java/cmput402/tdd/AppTest.java
+++ src/test/java/cmput402/tdd/AppTest.java
@@ -1,15 +1,26 @@
 package cmput402.tdd;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+
+import cmput402.tdd.service.Search;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
     extends TestCase
-{
+    
+    {
+	Utility util = new Utility();
+	
     /**
      * Create the test case
      *
@@ -34,5 +45,63 @@ public class AppTest
     public void testApp()
     {
         assertTrue( true );
+    }
+
+    public void testCountAverageScore() {
+    	Search mockSearch = mock(Search.class);
+    	HashMap<String, ArrayList<Integer>> fakeMap = new HashMap<String, ArrayList<Integer>>();
+    	
+    	ArrayList<Integer> messi = new ArrayList<Integer>();
+    	messi.add(100);
+    	messi.add(200);
+    	
+    	ArrayList<Integer> ronald = new ArrayList<Integer>();
+    	ronald.add(80);
+    	ronald.add(150);   	
+    	
+    	ArrayList<Integer> neymor = new ArrayList<Integer>();
+    	neymor.add(50);
+    	neymor.add(80);
+    	
+    	fakeMap.put("messi", messi);
+    	fakeMap.put("ronald", ronald);
+    	fakeMap.put("neymor", neymor);
+    	
+    	when(mockSearch.returnAll()).thenReturn(fakeMap);
+    	
+    	HashMap<String, Float> results = util.countAverageScore(mockSearch);
+    	
+    	assert(2==results.get("messi"));
+    	assert(1.875f==results.get("ronald"));
+    	assert(1.6f==results.get("neymor"));
+    }
+    
+    public void testFindTopScorer() {
+    	Integer maxScore;
+    	Search mockSearch = mock(Search.class);
+    	HashMap<String, ArrayList<Integer>> fakeMap = new HashMap<String, ArrayList<Integer>>();
+    	
+    	ArrayList<Integer> messi = new ArrayList<Integer>();
+    	messi.add(100);
+    	messi.add(200);
+    	
+    	ArrayList<Integer> ronald = new ArrayList<Integer>();
+    	ronald.add(80);
+    	ronald.add(150);   	
+    	
+    	ArrayList<Integer> neymor = new ArrayList<Integer>();
+    	neymor.add(50);
+    	neymor.add(80);
+    	
+    	fakeMap.put("messi", messi);
+    	fakeMap.put("ronald", ronald);
+    	fakeMap.put("neymor", neymor);
+    	
+    	when(mockSearch.returnAll()).thenReturn(fakeMap);
+//    	
+//    	HashMap<String, Float> results = util.findTopScorer(mockSearch);
+//    	
+//    	assert("messi" == results.get("messi"));
+
     }
 }
